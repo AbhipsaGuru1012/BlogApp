daf8bdd4fd161127f8135c0ed2be2c9d624e1d77
root <abhipsaguru@gmail.com>
2020-10-17 15:10:02 +0000

    made changes

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ app.delete("/blogs/:id",function(req, res){
 	})
 	
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT, process.env.IP,function(){
 	console.log("Server is listening at port 3000");
 })
